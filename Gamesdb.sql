3d0853489b62d4c200fcc3ec4df532e868757263
hella <ogobenehella@gmail.com>
2023-06-25 23:55:30 +0100

    HO: created table in gamesdb database

--- Gamesdb.sql
+++ Gamesdb.sql
@@ -1,1 +1,31 @@
 CREATE DATABASE IF NOT EXISTS gamesdb;
+
+USE gamesdb;
+CREATE TABLE IF NOT EXISTS customers(
+    customer_id INT NOT NULL AUTO_INCREMENT,
+    name varchar(40) NOT NULL,
+    email varchar(60) NOT NULL,
+    house_number int NOT NULL,
+    post_code varchar(8) NOT NULL,
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS products(
+    product_id INT NOT NULL AUTO_INCREMENT,
+    title varchar(40) NOT NULL,
+    price INT NOT NULL,
+    stock INT NOT NULL,
+    PRIMARY KEY (product_id)
+    );
+    
+CREATE TABLE orders(
+   order_id INT NOT NULL AUTO_INCREMENT,
+   customer_id INT NOT NULL,
+   product_id INT NOT NULL,
+   date_placed DATE NOT NULL,
+   PRIMARY KEY (order_id),
+   FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+   FOREIGN KEY (product_id) REFERENCES products(product_id)
+    );
+
+
